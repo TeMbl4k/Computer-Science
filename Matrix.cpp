49952c1c494e6c40e6079fc38eca5621ec9df2cf
TeMbl4 <artemgalkin426226@gmail.com>
2023-10-16 19:58:21 +0300

    1958

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,81 +1,83 @@
-//#include <iostream>
-//#include <random>
-//#include <iomanip>
-//
-//using namespace std;
-//
-//int main() {
-//	int a[10][10];
-//
-//	int value;
-//
-//	int min_value;
-//	cout << "Enter start of range: ";
-//	cin >> min_value;
-//
-//	int max_value;
-//	cout << "Enter end of range: ";
-//	cin >> max_value;
-//
-//	int sum = 0;
-//
-//	double s_i = 0;
-//	double s_j = 0;
-//
-//	random_device rd;
-//	mt19937 gen(rd());
-//	
-//	uniform_int_distribution <> dis(min_value, max_value);
-//
-//	 for (int i = 0; i < 10; ++i)  // Заполняем матрицу
-//	 {
-//		 for (int j = 0; j < 10; ++j) 
-//		 {
-//			 a[i][j] = dis(gen);
-//		 }
-//	 }
-//
-//	 for (int i = 0; i < 10; ++i)  // Сумма эл. матрицы
-//	 {
-//		 for (int j = 0; j < 10; ++j)
-//		 {
-//			 sum += a[i][j];
-//		 }
-//	 }
-//
-//	 cout << "---------------------------------------Matrix---------------------------------------- " << endl;
-//
-//	 cout << fixed << setprecision(2) << setw(5) << sum / 100; // Вывод ср.арифм. матрицы
-//
-//	 for (int i = 0; i < 10; ++i) // Получаем сумму каждого столбца и выводим ср. знач.
-//	 {
-//		 s_i = 0;
-//		 
-//		 for (int j = 0; j < 10; ++j)
-//		 {
-//			 s_i += a[j][i];
-//		 }
-//
-//		 cout << fixed << setprecision(2) << setw(8) << s_i/10;
-//	 }
-//
-//	 cout << endl;
-//
-//	 for (int i = 0; i < 10; ++i)  // Выводим матрицу
-//	 {
-//		 s_j = 0;
-//
-//		 for (int j = 0; j < 10; ++j) // Получаем сумму каждой строки и выводим ср. знач.
-//		 {
-//			 s_j += a[i][j];
-//		 }
-//
-//		 cout << fixed << setprecision(2) << setw(5) << s_j/10;
-//
-//		 for (int j = 0; j < 10; ++j)
-//		 {
-//			 cout << setw(8) << a[i][j];
-//		 }
-//		 cout << endl;
-//	 }
-//}
+#include <iostream>
+#include <random>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	const int matrix_size = 10;
+
+	int a[matrix_size][matrix_size];
+
+	int value = 0;
+
+	int min_value;
+	cout << "Enter start of range: ";
+	cin >> min_value;
+
+	int max_value;
+	cout << "Enter end of range: ";
+	cin >> max_value;
+
+	int sum = 0;
+
+	double s_i = 0;
+	double s_j = 0;
+
+	random_device rd;
+	mt19937 gen(rd());
+
+	uniform_int_distribution <> dis(min_value, max_value);
+
+	for (int i = 0; i < matrix_size; ++i) 
+	{
+		for (int j = 0; j < matrix_size; ++j)
+		{
+			a[i][j] = dis(gen);
+		}
+	}
+
+	for (int i = 0; i < matrix_size; ++i)
+	{
+		for (int j = 0; j < matrix_size; ++j)
+		{
+			sum += a[i][j];
+		}
+	}
+
+	cout << "----------------------------------------Matrix---------------------------------------- " << endl;
+
+	cout << fixed << setprecision(2) << setw(6) << sum / static_cast <double>(matrix_size * matrix_size);
+
+	for (int i = 0; i < matrix_size; ++i)
+	{
+		s_i = 0;
+
+		for (int j = 0; j < matrix_size; ++j)
+		{
+			s_i += a[j][i];
+		}
+
+		cout << fixed << setprecision(2) << setw(8) << s_i / matrix_size;
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < matrix_size; ++i)
+	{
+		s_j = 0;
+
+		for (int j = 0; j < matrix_size; ++j)
+		{
+			s_j += a[i][j];
+		}
+
+		cout << fixed << setprecision(2) << setw(6) << s_j / matrix_size;
+
+		for (int j = 0; j < matrix_size; ++j)
+		{
+			cout << setw(8) << a[i][j];
+		}
+		cout << endl;
+	}
+}
--- Stock.cpp
+++ Stock.cpp
@@ -1,88 +1,88 @@
-#include <iostream>
-#include <random>
-#include <iomanip>
-
-using namespace std;
-
-int main() {
-	int min_stock_price = 10;
-	int max_stock_price = 300000;
-
-	int div_stock_price = 0;
-
-	int cur_stock_price = 150000;
-
-	random_device rd;
-	mt19937 gen(rd());
-
-	uniform_int_distribution <> dis(-50000, 50000);
-
-	cout << noshowpos << setw(2) << 1 << ") " << setw(6) << cur_stock_price << "$" << " : ";
-	cout << setw(6) << showpos << div_stock_price << "$ |";
-
-	for (int i = 1; i <= (cur_stock_price / 5000); ++i) // Цикл для вывода i количества звёзд
-	{
-		cout << "*";
-	}
-	cout << endl;
-
-	div_stock_price = dis(gen);
-
-	for (int i = 2; i <= 36; ++i)
-	{
-		if ((10 < (cur_stock_price + div_stock_price)) && ((cur_stock_price + div_stock_price) < 300000))
-		{
-			cur_stock_price += div_stock_price;
-
-			cout << noshowpos << setw(2) << i << ") " << setw(6) << cur_stock_price << "$" << " : ";
-			cout << setw(6) << showpos << div_stock_price << "$ |";
-
-			for (int i = 1; i <= (cur_stock_price / 5000); ++i) // Цикл для вывода i количества звёзд
-			{
-				cout << "*";
-			}
-			cout << endl;
-
-			div_stock_price = dis(gen);
-		}
-
-		else if (10 > (cur_stock_price + div_stock_price))
-		{
-			int dif = div_stock_price + 50000;
-			div_stock_price = dif;
-
-			cur_stock_price += div_stock_price;
-
-			cout << noshowpos << setw(2) << i << ") " << setw(6) << cur_stock_price << "$" << " : ";
-			cout << setw(6) << showpos << div_stock_price << "$ |";
-
-			for (int i = 1; i <= (cur_stock_price / 5000); ++i) // Цикл для вывода i количества звёзд
-			{
-				cout << "*";
-			}
-			cout << endl;
-
-			div_stock_price = dis(gen);
-		}
-
-		else 
-		{
-			int dif = div_stock_price - 50000;
-			div_stock_price = dif;
-
-			cur_stock_price += div_stock_price;
-
-			cout << noshowpos << setw(2) << i << ") " << setw(6) << cur_stock_price << "$" << " : ";
-			cout << setw(6) << showpos << div_stock_price << "$ |";
-
-			for (int i = 1; i <= (cur_stock_price / 5000); ++i) // Цикл для вывода i количества звёзд
-			{
-				cout << "*";
-			}
-			cout << endl;
-
-			div_stock_price = dis(gen);
-		}
-	}
-}
-
+//#include <iostream>
+//#include <random>
+//#include <iomanip>
+//
+//using namespace std;
+//
+//int main() {
+//	int min_stock_price = 10;
+//	int max_stock_price = 300000;
+//
+//	int div_stock_price = 0;
+//
+//	int cur_stock_price = 150000;
+//
+//	random_device rd;
+//	mt19937 gen(rd());
+//
+//	uniform_int_distribution <> dis(-50000, 50000);
+//
+//	cout << noshowpos << setw(2) << 1 << ") " << setw(6) << cur_stock_price << "$" << " : ";
+//	cout << setw(6) << showpos << div_stock_price << "$ |";
+//
+//	for (int i = 1; i <= (cur_stock_price / 5000); ++i) // Цикл для вывода i количества звёзд
+//	{
+//		cout << "*";
+//	}
+//	cout << endl;
+//
+//	div_stock_price = dis(gen);
+//
+//	for (int i = 2; i <= 36; ++i)
+//	{
+//		if ((10 < (cur_stock_price + div_stock_price)) && ((cur_stock_price + div_stock_price) < 300000))
+//		{
+//			cur_stock_price += div_stock_price;
+//
+//			cout << noshowpos << setw(2) << i << ") " << setw(6) << cur_stock_price << "$" << " : ";
+//			cout << setw(6) << showpos << div_stock_price << "$ |";
+//
+//			for (int i = 1; i <= (cur_stock_price / 5000); ++i) // Цикл для вывода i количества звёзд
+//			{
+//				cout << "*";
+//			}
+//			cout << endl;
+//
+//			div_stock_price = dis(gen);
+//		}
+//
+//		else if (10 > (cur_stock_price + div_stock_price))
+//		{
+//			int dif = div_stock_price + 50000;
+//			div_stock_price = dif;
+//
+//			cur_stock_price += div_stock_price;
+//
+//			cout << noshowpos << setw(2) << i << ") " << setw(6) << cur_stock_price << "$" << " : ";
+//			cout << setw(6) << showpos << div_stock_price << "$ |";
+//
+//			for (int i = 1; i <= (cur_stock_price / 5000); ++i) // Цикл для вывода i количества звёзд
+//			{
+//				cout << "*";
+//			}
+//			cout << endl;
+//
+//			div_stock_price = dis(gen);
+//		}
+//
+//		else 
+//		{
+//			int dif = div_stock_price - 50000;
+//			div_stock_price = dif;
+//
+//			cur_stock_price += div_stock_price;
+//
+//			cout << noshowpos << setw(2) << i << ") " << setw(6) << cur_stock_price << "$" << " : ";
+//			cout << setw(6) << showpos << div_stock_price << "$ |";
+//
+//			for (int i = 1; i <= (cur_stock_price / 5000); ++i) // Цикл для вывода i количества звёзд
+//			{
+//				cout << "*";
+//			}
+//			cout << endl;
+//
+//			div_stock_price = dis(gen);
+//		}
+//	}
+//}
+//
