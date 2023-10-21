c738087c36b24b3a42465106c9d221610b8a0aec
TeMbl4 <artemgalkin426226@gmail.com>
2023-10-21 11:26:51 +0300

    square

--- square.cpp
+++ square.cpp
@@ -7,15 +7,10 @@ int main() {
 	cout << "Enter amount of numbers: ";
 	cin >> num_count;
 
-
-	// Выделяем динамически память для массива чисел
 	int* numbers = new int[num_count];
 
-	// Получаем числа от пользователя и добавляем их в массив
-	cout << "Enter numbers:" << endl;
 	for (int i = 0; i < num_count; ++i) {
-		cout << i + 1 <<  ") ";
-		cin >> numbers[i];
+		cout << i + 1 << ") "; cin >> numbers[i];
 	}
 
 	// Выделяем динамически память для двумерного массива квадрата
@@ -24,19 +19,43 @@ int main() {
 		result[i] = new int[num_count];
 	}
 
+	for (int i = 0; i < num_count; ++i) {
+		for (int j = 0; j < num_count; ++j) {
+			result[i][j] = 0;
+		}
+	}
 
 	// Заполняем квадрат числами согласно условиям задачи
 	for (int i = 0; i < num_count; ++i) {
 		for (int j = 0; j < num_count; ++j)
-				result[i][j] = numbers[j];
+		{
+			if ((i == 0) || (j == 0)) {
+				if (i == 0)
+					result[i][j] = numbers[j];
+
+				if (j == 0)
+					result[i][j] = numbers[i];
+			}
+
+			else {
+				if ((i == num_count - 1) && (j != 0))
+					result[i][j] = numbers[num_count - j - 1];
+
+				if ((j == num_count - 1) && (i != 0))
+					result[i][j] = numbers[num_count - i - 1];
+			}
 		}
 	}
 
 	// Выводим результат
 	cout << "- Result -" << endl;
 	for (int i = 0; i < num_count; ++i) {
-		for (int j = 0; j < num_count; ++j) {
-			cout << result[i][j] << " ";
+		for (int j = 0; j < num_count; ++j)
+		{
+			if (result[i][j] == 0)
+				cout << "  ";
+			else
+				cout << result[i][j] << " ";
 		}
 		cout << endl;
 	}
