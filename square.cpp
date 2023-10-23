6ab555de54ada5ce3c4cf9714e70c6800038b4dc
TeMbl4 <artemgalkin426226@gmail.com>
2023-10-23 21:28:18 +0300

    23.10

--- square.cpp
+++ square.cpp
@@ -1,37 +1,42 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//    int numCount;
-//    cout << "Enter amount of numbers: ";
-//    cin >> numCount;
-//
-//    int* numbers = new int[numCount];
-//
-//    for (int i = 0; i < numCount; ++i) {
-//        cout << i + 1 << ") ";
-//        cin >> numbers[i];
-//    }
-//
-//    for (int i = 0; i < numCount; ++i) {
-//        cout << numbers[i] << " ";
-//    }
-//    cout << endl;
-//
-//    for (int i = 1; i < numCount - 1; ++i) {
-//        cout << numbers[i] << " ";
-//        for (int j = 1; j < numCount - 1; ++j) {
-//            cout << "  ";
-//        }
-//        cout << numbers[numCount - i - 1] << endl;
-//    }
-//
-//    for (int i = 0; i < numCount; ++i) {
-//        cout << numbers[numCount - i - 1] << " ";
-//    }
-//    cout << endl;
-//
-//    delete[] numbers;
-//
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numCount;
+    cout << "Enter amount of numbers: ";
+    cin >> numCount;
+
+    int* numbers = new int[numCount];
+
+    for (int i = 0; i < numCount; ++i) {
+        cout << i + 1 << ") ";
+        cin >> numbers[i];
+    }
+    
+    if (numCount != 1) {
+        for (int i = 0; i < numCount; ++i) {
+            cout << numbers[i] << " ";
+        }
+        cout << endl;
+
+        for (int i = 1; i < numCount - 1; ++i) {
+            cout << numbers[i] << " ";
+            for (int j = 1; j < numCount - 1; ++j) {
+                cout << "  ";
+            }
+            cout << numbers[numCount - i - 1] << endl;
+        }
+
+        for (int i = 0; i < numCount; ++i) {
+            cout << numbers[numCount - i - 1] << " ";
+        }
+        cout << endl;
+    }
+
+    else {
+
+        cout << numbers[0];
+    }
+
+    delete[] numbers;
+}
--- template.cpp
+++ template.cpp
@@ -1,49 +1,49 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    // ¬вод данных
-    int L, Rn, Cn;
-    cout << "Enter L: ";
-    cin >> L;
-
-    int* numbers = new int[L];
-    cout << "Enter numbers:" << endl;
-    for (int i = 0; i < L; ++i) {
-        cout << "T" << i + 1 << ") ";
-        cin >> numbers[i];
-    }
-    cout << "Enter Rn: ";
-    cin >> Rn;
-    cout << "Enter Cn: ";
-    cin >> Cn;
-
-    // ѕечать рисунка
-    int patternLength = L * Cn;
-    for (int r = 0; r < Rn; ++r) {
-        for (int i = 0; i < patternLength; ++i) {
-           if (numbers[i % L] == 0) {
-                 cout << " ";
-           }
-           else if (numbers[i % L] == 1) {
-                cout << "*";
-           }
-           else if (numbers[i] == 2) {
-                 cout << "\\";
-           }
-           else if (numbers[i] == 3) {
-                cout << "/";
-           }
-           else if (numbers[i] == 4) {
-                cout << "+";
-           }
-           else if (numbers[i] == 5) {
-               cout << "#";
-           }
-        }
-        cout << endl;
-    }
-
-    return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    // ¬вод данных
+//    int L, Rn, Cn;
+//    cout << "Enter L: ";
+//    cin >> L;
+//
+//    int* numbers = new int[L];
+//    cout << "Enter numbers:" << endl;
+//    for (int i = 0; i < L; ++i) {
+//        cout << "T" << i + 1 << ") ";
+//        cin >> numbers[i];
+//    }
+//    cout << "Enter Rn: ";
+//    cin >> Rn;
+//    cout << "Enter Cn: ";
+//    cin >> Cn;
+//
+//    // ѕечать рисунка
+//    int patternLength = L * Cn;
+//    for (int r = 0; r < Rn; ++r) {
+//        for (int i = 0; i < patternLength; ++i) {
+//           if (numbers[i % L] == 0) {
+//                 cout << " ";
+//           }
+//           else if (numbers[i % L] == 1) {
+//                cout << "*";
+//           }
+//           else if (numbers[i] == 2) {
+//                 cout << "\\";
+//           }
+//           else if (numbers[i] == 3) {
+//                cout << "/";
+//           }
+//           else if (numbers[i] == 4) {
+//                cout << "+";
+//           }
+//           else if (numbers[i] == 5) {
+//               cout << "#";
+//           }
+//        }
+//        cout << endl;
+//    }
+//
+//    return 0;
+//}
