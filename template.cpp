e9c9ee8d06de1318876570c138fcc5036157ccbf
TeMbl4 <artemgalkin426226@gmail.com>
2023-10-30 19:55:31 +0300

    30.10

--- template.cpp
+++ template.cpp
@@ -1,90 +1,58 @@
-////#include <iostream>
-////
-////using namespace std;
-////
-////int main() {
-////    // ¬вод данных
-////    int L, Rn, Cn;
-////    cout << "Enter L: ";
-////    cin >> L;
-////
-////    int* numbers = new int[L];
-////    cout << "Enter numbers:" << endl;
-////    for (int i = 0; i < L; ++i) {
-////        cout << "T" << i + 1 << ") ";
-////        cin >> numbers[i];
-////    }
-////    cout << "Enter Rn: ";
-////    cin >> Rn;
-////    cout << "Enter Cn: ";
-////    cin >> Cn;
-////
-////    const char symbolChars[] = { ' ', '*', '\\', '/', '+', '#' };
-////
-////    int d = 0;
-////
-////    // ѕечать рисунка
-////    for (int j = 0; j < L; ++j) {
-////        for (int i = 0; i < L; ++i) {
-////            if ((i + j) < L)
-////                cout << symbolChars[numbers[i + j]];
-////
-////        }
-////        cout << endl;
-////    }
-////}
-//
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//    int L, Rn, Cn;
-//    cout << "Enter L: ";
-//    cin >> L;
-//
-//    cout << "Enter numbers:" << endl;
-//    int ch;
-//    string str = "";
-//    for (int i = 0; i < L; i++) {
-//        cout << "T" << i + 1 << ") ";
-//        cin >> ch;
-//         if (ch == 0) {
-//                str += " ";
-//            }
-//            else if (ch == 1) {
-//                str += "*";
-//            }
-//            else if (ch == 2) {
-//                str+= "\\";
-//            }
-//            else if (ch == 3) {
-//                str+="/";
-//            }
-//            else if (ch == 4) {
-//                str+= "+";
-//            }
-//            else {
-//                str+= "#";
-//    }
-//    }
-//    cout << "Enter Rn: ";
-//    cin >> Rn;
-//    cout << "Enter Cn: ";
-//    cin >> Cn;
-//
-//    int d = 0;
-// for (int k = 0; k <Rn;k++){
-//     for (int j = 0; j <L; j++) {
-//         for (int i = 0; i < L; i++) {
-//             if (d>L)
-//                 d = 0;
-//    cout << str[d];
-//          
-//          d++;
-//         }
-//         cout << endl;
-//     }
-//    }
-//
-//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int L, Rn, Cn, ch;
+    cout << "Enter L: ";
+    cin >> L;
+
+	char* str = new char[L];
+
+
+	cout << "Enter numbers:" << endl;
+    for (int i = 0; i < L; i++) {
+        cout << "T" << i + 1 << ") ";
+        cin >> ch;
+		switch (ch)
+		{
+		case(0):
+			str[i] = ' ';
+			break;
+		case(1):
+			str[i] = '*';
+			break;
+		case(2):
+			str[i] = '\\';
+			break;
+		case(3):
+			str[i] = '/';
+			break;
+		case(4):
+			str[i] = '+';
+			break;
+		case(5):
+			str[i] = '#';
+			break;
+		default:
+			break;
+		}
+    }
+
+	cout << "Enter Rn: ";
+	cin >> Rn;
+	cout << "Enter Cn: ";
+	cin >> Cn;
+
+	for (int i = 0; i < Rn; i++) {
+		for (int j = 0; j < L; j++) {
+			for (int k = 0; k < Cn; k++) {
+				for (int d = 0; d < L; d++) {
+					cout << str[(j - d + L - 1) % L];
+				}
+			}
+			cout << endl;
+		}
+	}
+	delete[] str;
+}
