a2c832eb7752cc5fda878e8964daa13ad05e8727
TeMbl4 <artemgalkin426226@gmail.com>
2023-11-01 19:31:52 +0300

    01.11

--- randomString.cpp
+++ randomString.cpp
@@ -1,69 +1,59 @@
-//#include <iostream>
-//#include <random>
-//
-//using namespace std;
-//
-//char getRandomChar() {
-//    int randomNum = rand() % 26;
-//    return static_cast<char>(randomNum + 65);
-//}
-//
-//void printSubstrings(int N, int* lengths) {
-//    cout << "- Substrings -" << endl;
-//
-//    for (int i = 0; i < N; i++) {
-//        cout << "N[" << i << "] = ";
-//
-//        for (int j = 0; j < lengths[i]; j++) {
-//            cout << getRandomChar();
-//
-//        }
-//
-//        cout << " ";
-//    }
-//
-//    cout << endl;
-//}
-//
-//void printResult(int L, int* indices, int N, int* lengths) {
-//    cout << "- Result - " << endl;
-//
-//    for (int i = 0; i < L; i++) {
-//        int index = indices[i];
-//
-//        if (index >= 0 && index < N) {
-//            for (int j = 0; j < lengths[index]; j++) {
-//                cout << getRandomChar();
-//            }
-//        }
-//    }
-//
-//    cout << endl;
-//}
-//
-//int main() {
-//    int N;
-//    cout << "Enter N: ";
-//    cin >> N;
-//
-//    int* lengths = new int[N];
-//    for (int i = 0; i < N; i++) {
-//        cout << "N" << i + 1 << "s) ";
-//        cin >> lengths[i];
-//    }
-//
-//    printSubstrings(N, lengths);
-//
-//    int L;
-//    cout << "Enter L: ";
-//    cin >> L;
-//
-//    int* indices = new int[L];
-//    for (int i = 0; i < L; i++) {
-//        cout << "L" << i + 1 << ") ";
-//        cin >> indices[i];
-//    }
-//
-//    printResult(L, indices, N, lengths);
-//
-//}
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main() {
+    int N, L, lenght;
+    cout << "Enter N: ";
+    cin >> N;
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dis('A', 'Z');
+
+    char** array = new char* [N];
+    for (int i = 0; i < N; ++i) {
+        cout << "N" << i + 1 << "s) ";
+        cin >> L;
+        array[i] = new char[L];
+
+        for (int j = 0; j < L; ++j) {
+            array[i][j] = static_cast<char>(dis(gen));
+        }
+    }
+
+    cout << "- Substrings -" << endl;
+    for (int i = 0; i < N; ++i) {
+        cout << "N[" << i << "] = ";
+        for (int j = 0; j < array[i][j] != '\0'; ++j) {
+            cout << array[i][j];
+        }
+        cout << endl;
+    }
+
+    cout << "Enter L: ";
+    cin >> lenght;
+
+    int num;
+    int* nums = new int[lenght];
+
+    for (int i = 0; i < lenght; ++i) {
+        cout << "L" << i + 1 << ") ";
+        cin >> nums[i];
+    }
+
+    cout << "- Result -" << endl;
+    for (int i = 0; i < lenght; ++i) {
+        for (int j = 0; j < array[nums[i]][j] != '\0'; ++j) {
+            cout << array[nums[i]][j];
+        }
+    }
+
+
+    for (int i = 0; i < N; ++i) {
+        delete[] array[i];
+    }
+    delete[] array;
+
+}
