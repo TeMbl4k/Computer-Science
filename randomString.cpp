a87329a44330d534116f5d5cc13c80c98e9dde12
TeMbl4 <artemgalkin426226@gmail.com>
2023-11-01 19:33:54 +0300

    01.11

--- randomString.cpp
+++ randomString.cpp
@@ -4,60 +4,56 @@
 //using namespace std;
 //
 //int main() {
-//    random_device rd;
-//    mt19937 gen(rd());
-//    uniform_int_distribution<int> charDistribution('A', 'Z');
-//
-//    int N;
+//    int N, L, lenght;
 //    cout << "Enter N: ";
 //    cin >> N;
 //
-//    int* substring_lengths = new int[N];
-//    string substrings[N];
+//    random_device rd;
+//    mt19937 gen(rd());
+//    uniform_int_distribution<int> dis('A', 'Z');
 //
+//    char** array = new char* [N];
 //    for (int i = 0; i < N; ++i) {
-//        int length;
-//        cout << "N" << (i + 1) << "s) ";
-//        cin >> length;
+//        cout << "N" << i + 1 << "s) ";
+//        cin >> L;
+//        array[i] = new char[L];
 //
-//        string substring;
-//        for (int j = 0; j < length; ++j) {
-//            char random_char = charDistribution(gen);
-//            substring.push_back(random_char);
+//        for (int j = 0; j < L; ++j) {
+//            array[i][j] = static_cast<char>(dis(gen));
 //        }
-//
-//        substrings[i] = substring;
-//        substring_lengths[i] = length;
 //    }
 //
 //    cout << "- Substrings -" << endl;
 //    for (int i = 0; i < N; ++i) {
-//        cout << "N[" << i << "] = " << substrings[i] << endl;
+//        cout << "N[" << i << "] = ";
+//        for (int j = 0; j < array[i][j] != '\0'; ++j) {
+//            cout << array[i][j];
+//        }
+//        cout << endl;
 //    }
 //
-//    int L;
 //    cout << "Enter L: ";
-//    cin >> L;
+//    cin >> lenght;
 //
-//    int indices[L];
-//    for (int i = 0; i < L; ++i) {
-//        int index;
-//        cout << "L" << (i + 1) << ") ";
-//        cin >> index;
+//    int num;
+//    int* nums = new int[lenght];
 //
-//        if (index >= 0 && index < N) {
-//            indices[i] = index;
-//        }
-//        else {
-//            cout << "Invalid index. Please enter a valid index." << endl;
-//            --i;
-//        }
+//    for (int i = 0; i < lenght; ++i) {
+//        cout << "L" << i + 1 << ") ";
+//        cin >> nums[i];
 //    }
 //
 //    cout << "- Result -" << endl;
-//    for (int i = 0; i < L; ++i) {
-//        cout << substrings[indices[i]];
+//    for (int i = 0; i < lenght; ++i) {
+//        for (int j = 0; j < array[nums[i]][j] != '\0'; ++j) {
+//            cout << array[nums[i]][j];
+//        }
 //    }
 //
-//    cout << endl;
+//
+//    for (int i = 0; i < N; ++i) {
+//        delete[] array[i];
+//    }
+//    delete[] array;
+//
 //}
