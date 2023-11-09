f98c527b0c552f3feeadef67e3aab31b5ca5a339
TeMbl4 <artemgalkin426226@gmail.com>
2023-11-09 15:04:04 +0300

    09.11

--- Index.cpp
+++ Index.cpp
@@ -1,57 +1,47 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//bool finder(int value, int array_size, int* array, int& index) {
-//    for (int i = 0; i < array_size; ++i) {
-//        if (value == array[i]) {
-//            index = i;
-//            return true;
-//        }
-//    }
-//    return false;
-//}
-//
-//int main() {
-//    int array_size, value1, value2;
-//
-//    cout << "Enter array size: ";
-//    cin >> array_size;
-//
-//    int* nums = new int[array_size];
-//
-//    for (int i = 0; i < array_size; ++i) {
-//        cout << "A" << i + 1 << ") ";
-//        cin >> nums[i];
-//    }
-//
-//    cout << "Enter first value: ";
-//    cin >> value1;
-//
-//    int index1;
-//    bool found1 = finder(value1, array_size, nums, index1);
-//
-//    cout << "Index of first value: ";
-//    if (found1) {
-//        cout << index1 << endl;
-//    }
-//    else {
-//        cout << "null" << endl;
-//    }
-//
-//    cout << "Enter second value: ";
-//    cin >> value2;
-//
-//    int index2;
-//    bool found2 = finder(value2, array_size, nums, index2);
-//
-//    cout << "Index of second value: ";
-//    if (found2) {
-//        cout << index2 << endl;
-//    }
-//    else {
-//        cout << "null" << endl;
-//    }
-//    
-//    delete[] nums;
-//}
+#include <iostream>
+
+using namespace std;
+
+bool finder(int value, int array_size, int array[], int& index) {
+    for (int i = 0; i < array_size; ++i) {
+        if (value == array[i]) {
+            index = i;
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    int array_size, value, index;
+
+    cout << "Enter array size: ";
+    cin >> array_size;
+
+    int* nums = new int[array_size];
+
+    for (int i = 0; i < array_size; ++i) {
+        cout << "A" << i + 1 << ") ";
+        cin >> nums[i];
+    }
+
+    cout << "Enter first value: ";
+    cin >> value;
+
+    cout << "Index of first value: ";
+    if (finder(value, array_size, nums, index))
+        cout << index << endl;
+    else
+        cout << "null" << endl;
+
+    cout << "Enter second value: ";
+    cin >> value;
+
+    cout << "Index of second value: ";
+    if (finder(value, array_size, nums, index))
+        cout << index << endl;
+    else
+        cout << "null" << endl;
+    
+    delete[] nums;
+}
